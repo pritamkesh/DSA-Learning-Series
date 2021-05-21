dd5d600e543eab2e1ae3080f7c4e590cc2f227ef
Pritam Kesh <pritamkesh.summercode@gmail.com>
2021-05-21 23:05:01 +0530

    Creating the repo and first commit

---
+++ Easy_Problems_to_Get_Started/buy_please.py
@@ -0,0 +1,26 @@
+# cook your dish here
+def buy_please(a, b, x, y) -> int:
+    if a != 0 or x != 0:
+        pens_cost = a * x
+    else:
+        pens_cost = 0
+
+    if b != 0 or y != 0:
+        pencils_cost = b * y
+    else:
+        pencils_cost = 0
+
+    total_cost = pens_cost + pencils_cost
+    print(total_cost)
+
+
+if __name__ == '__main__':
+    a, b, x, y = input().split()
+    a, b = int(a, b)
+    x, y = int(x, y)
+    # a = int(a)
+    # b = int(b)
+    # x = int(x)
+    # y = int(y)
+    buy_please(a, b, x, y)
+    #buy_please(1,1,4,8)
