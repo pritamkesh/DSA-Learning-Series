480007f138fd0bb822c9070a39a3747f2c7a21a6
Pritam Kesh <pritamkesh.summercode@gmail.com>
2021-05-24 21:56:48 +0530

    Updating progress on alternative square pattern problem

--- Easy_Problems_to_Get_Started/alternative_square_pattern.py
+++ Easy_Problems_to_Get_Started/alternative_square_pattern.py
@@ -1,18 +1,18 @@
-def alternative_square_pattern(N) -> int:
-    pattern = """1 2 3 4 5
-10 9 8 7 6
-11 12 13 14 15
-20 19 18 17 16
-21 22 23 24 25
-30 29 28 27 26"""
-    numbers = []
-    for line in pattern:
-        numbers
+# Problem Statement : https://www.codechef.com/CCSTART2/problems/SQALPAT
+# cook your dish here (Yet to solve!)
+def alternative_square_pattern(no_of_lines, number_pattern) -> int:
 
-    for i in range(0, N):
-        print(numbers[i])
+    number_pattern_list = number_pattern.splitlines()
+    for i in range(0, no_of_lines):
+        print(number_pattern_list[i])
 
 
 if __name__ == "__main__":
     N = int(input())
-    alternative_square_pattern(N)
+    pattern = '1 2 3 4 5\n' \
+              '10 9 8 7 6\n' \
+              '11 12 13 14 15\n' \
+              '20 19 18 17 16\n' \
+              '21 22 23 24 25\n' \
+              '30 29 28 27 26\n'
+    alternative_square_pattern(N, pattern)
