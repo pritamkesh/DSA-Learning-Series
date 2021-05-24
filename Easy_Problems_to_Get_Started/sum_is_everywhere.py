34c34bdeb3483573ddc6addf718a1abdf5a64022
Pritam Kesh <pritamkesh.summercode@gmail.com>
2021-05-24 21:29:23 +0530

    Working solution of Sum is everywhere

--- Easy_Problems_to_Get_Started/sum_is_everywhere.py
+++ Easy_Problems_to_Get_Started/sum_is_everywhere.py
@@ -1,18 +1,20 @@
-# Yet to solve this problem
+# Problem Statement : https://www.codechef.com/CCSTART2/problems/SUMEVOD
+# cook your dish here
 def sum_is_everywhere(N) -> int:
     odd_sum = 0
     even_sum = 0
-    even_count = 1
-    odd_count = 0
-    i = 1
-    while even_count <= N:
-        for number in range(1, N):
-            if number % 2 == 0:
-                even_sum += number
-                even_count += 1
+    for number in range(1, 2*N+1):
+        if number % 2 == 0:
+            even_sum += number
+        else:
+            odd_sum += number
+    print(odd_sum, even_sum)
 
-    # print(odd_sum)
-    print(even_sum)
+    # -------------Mathematical way to solve this(Preferable) --------------- #
+    # # Using direct mathematical formula :
+    # sum_of_even_no = N*(N+1)
+    # sum_of_odd_no = N*N
+    # print(sum_of_odd_no, sum_of_even_no)
 
 
 if __name__ == "__main__":
